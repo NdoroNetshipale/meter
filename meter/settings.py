38aaeaf270b2c1dbc70f7800ef7da8eabe87fe31
NdoroNetshipale <52962982+NdoroNetshipale@users.noreply.github.com>
2020-09-26 13:16:07 +0200

    Update settings.py

--- meter/settings.py
+++ meter/settings.py
@@ -13,7 +13,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django
+import django_heroku
 django.setup()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -124,4 +126,5 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATIC_URL = '/static/'
+django_heroku.settings(locals())
 

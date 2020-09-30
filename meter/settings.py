e19f82c9581c7190ded6cf7b92d5fa8d4299e6ba
NdoroNetshipale <52962982+NdoroNetshipale@users.noreply.github.com>
2020-09-30 11:40:54 +0200

    Update settings.py

--- meter/settings.py
+++ meter/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'qa#^!-q*_i)b&_&%#)!-(3a6u147e&z7fww_rzby%+x$l+3d$d'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
